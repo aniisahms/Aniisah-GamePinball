6c3c0b51890daf7cefecd55bdd6927ad805c2809
aniisahms <anisahmufida@gmail.com>
2023-04-30 12:04:13 +0700

    UI & Scene Management (+)

--- Pinball/Assets/Scripts/GameOverUIController.cs
+++ Pinball/Assets/Scripts/GameOverUIController.cs
@@ -7,14 +7,21 @@ using UnityEngine.UI;
 public class GameOverUIController : MonoBehaviour
 {
     public Button mainMenuButton;
+    public Button replayButton;
 
     private void Start() {
         gameObject.SetActive(false);
         mainMenuButton.onClick.AddListener(BackToMainMenu);
+        replayButton.onClick.AddListener(PlayAgain);
     }
 
     private void BackToMainMenu()
     {
         SceneManager.LoadScene("MainMenuScene");
+    }
+
+    private void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
